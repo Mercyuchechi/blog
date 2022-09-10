61a786264fddc047d4aeef498c76cf8148cd516d
Mercyuchechi <112502776+Mercyuchechi@users.noreply.github.com>
2022-09-10 06:15:01 -0700

    this is where i change some files

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+// import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return ( 
+        <div>
+            <div>
+                <h4>My Blog</h4>
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Register</li>
+                </ul>
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
